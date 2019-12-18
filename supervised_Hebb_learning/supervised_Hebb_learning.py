13d8ac6857367e11877bb07e0697ea25d41669d4
Tony-Tan <363806348@qq.com>
2019-12-18 18:14:15 +0800

    Hebb learning app

--- supervised_Hebb_learning/supervised_Hebb_learning.py
+++ supervised_Hebb_learning/supervised_Hebb_learning.py
@@ -23,16 +23,17 @@ class HebbLearning():
         """
         initial function
         :param training_data_path: the path of training set and their labels.
-        They should be organized as pictures in '.png' form  whose names are its label
+        They should be organized as pictures in '.png' form
         :param gamma: the punishment coefficients
         :param alpha: learning rate
         """
         self.gamma = gamma
         self.alpha = alpha
-        x,labels = self.load_data(training_data_path)
+        x = self.load_data(training_data_path)
         self.X = np.array(x)
-        self.label = np.array(labels)
-        self.weights = np.zeros((np.shape(labels)[1], np.shape(x)[1]))
+        self.label = np.array(x)
+        self.weights = np.zeros((np.shape(x)[1], np.shape(x)[1]))
+        self.test_data = []
 
     def load_data(self, data_path):
         """
@@ -40,30 +41,22 @@ class HebbLearning():
         :param data_path: the path of data
         :return: training set and targets respectively
         """
-        training_files_name_list = os.listdir(data_path)
+        name_list = os.listdir(data_path)
         X = []
-        labels = []
-        for file_name in training_files_name_list:
-            training_data = cv2.imread(os.path.join(data_path, file_name), 0)
-            if training_data is None:
+        for file_name in name_list:
+            data = cv2.imread(os.path.join(data_path, file_name), 0)
+            if data is None:
                 continue
             else:
-                training_data = training_data.reshape(1, -1)[0].astype(np.float64)
-            for i in range(len(training_data)):
-                if training_data[i] > 0:
-                    training_data[i] = 1
+                data = data.reshape(1, -1)[0].astype(np.float64)
+            for i in range(len(data)):
+                if data[i] > 0:
+                    data[i] = 1
                 else:
-                    training_data[i] = -1
-            X.append(training_data)
-            label = file_name.split('.')[0]
-            label_int = []
-            for i in range(len(label)):
-                if int(label[i]) == 0:
-                    label_int.append(-1)
-                else:
-                    label_int.append(1)
-            labels.append(label_int)
-        return X, labels
+                    data[i] = -1
+            data=data/np.linalg.norm(data)
+            X.append(data)
+        return X
 
     def process(self):
         """
@@ -79,16 +72,22 @@ class HebbLearning():
         :param input_path: test date should be organized as pictures whose names are its label
         :return: output label and
         """
-        X, labels = self.load_data(input_path)
+        self.test_data = self.load_data(input_path)
+
         labels_test = []
-        for x in X:
+        for x in self.test_data:
             output_origin = np.dot(self.weights,x.reshape(-1,1))
             labels_test.append(symmetrical_hard_limit(output_origin))
-        return np.array(labels_test), labels
+        return np.array(labels_test)
 
 
 if __name__ == '__main__':
     h = HebbLearning(alpha=1.0)
     h.process()
-    print(h.test()[0])
-    print(h.test()[1])
+    for i,x in zip(h.test(),h.test_data):
+        image_repair = i.reshape(6,5).astype(np.uint8)
+        image = x.reshape(6,5).astype(np.uint8)
+        cv2.imshow('image_repaired',image_repair)
+        cv2.imshow('image_org',image)
+        cv2.waitKey(0)
+        print(i)
